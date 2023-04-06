10bc0f7c475c123727f373b420e1766c7cdd4118
PAWAN KUMAR JHA <88606641+Pjha72@users.noreply.github.com>
2023-04-06 19:48:40 +0530

    Update StudentLogin.js

--- client/src/components/login/studentLogin/StudentLogin.js
+++ client/src/components/login/studentLogin/StudentLogin.js
@@ -45,6 +45,11 @@ const StudentLogin = () => {
   }, [store.errors]);
   return (
     <div className="bg-[#d65158] h-screen w-screen flex items-center justify-center">
+    <a href="/"><button
+            className="w-32 hover:scale-105 transition-all duration-150 rounded-lg flex items-right justify-center text-white text-base py-1 bg-[#FF2400]">
+            Home
+          </button>
+      </a>
       <div className="grid grid-cols-2">
         <div
           className={`h-96 w-96 bg-white flex items-center justify-center ${
