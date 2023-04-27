5e578213f6de4c60d1110e081752762da90d81de
Anand Jaiswar <anandjaiswar02@gmail.com>
2023-04-27 21:20:21 +0530

    Updates

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
       }}>
       <div className="flex flex-col items-center mt-10 space-y-32">
         <h1 className="text-3xl font-semibold bg-black text-white w-full text-center py-4 bg-opacity-75 rounded-2xl">
-          Apna College
+          THAKUR COLLEGE OF ENGINEERING AND TECHNOLOGY
         </h1>
         <div className="grid grid-cols-3 gap-28">
         <div className="h-96 w-96 space-y-11 shadow-2xl flex flex-col justify-center items-center bg-transparent backdrop-blur-md bg-[#E91E63] bg-opacity-60 rounded-xl">
