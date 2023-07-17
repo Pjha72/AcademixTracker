5fc3214a5309bdfbaaefda7a2307daa3d3e935d5
PAWAN KUMAR JHA <88606641+Pjha72@users.noreply.github.com>
2023-07-17 21:23:27 +0530

    Update Login.js

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -12,9 +12,13 @@ const Login = () => {
       }}
     >
       <div className="flex flex-col items-center mt-10 space-y-32">
-        <h1 className="text-3xl font-semibold bg-black text-white w-full text-center py-4 bg-opacity-75 rounded-2xl">
-          THAKUR COLLEGE OF ENGINEERING AND TECHNOLOGY
-        </h1>
+        <span className="flex mt-10 w-[65vw]">
+          <img src="https://qph.cf2.quoracdn.net/main-qimg-f7cfa5f20eb8584fbb73942e23c31bf4" className="rounded-full h-12 w-12"/>
+          <h1 className="text-3xl font-bold  text-white w-full text-center py-0 bg-opacity-100 rounded-3xl">
+              THAKUR COLLEGE OF ENGINEERING AND TECHNOLOGY
+          </h1>
+          <img src="https://qph.cf2.quoracdn.net/main-qimg-f7cfa5f20eb8584fbb73942e23c31bf4" className="rounded-full h-12 w-12"/>
+        </span>
         <div className="grid grid-cols-3 gap-28">
           <div className="h-96 w-96 space-y-11 shadow-2xl flex flex-col justify-center items-center bg-transparent backdrop-blur-md bg-[#E91E63] bg-opacity-60 rounded-xl">
             <h1 className="text-4xl font-extrabold">Admin</h1>
